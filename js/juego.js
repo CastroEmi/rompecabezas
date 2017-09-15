1f7f0cd94c178e1394e840b88a9ca0bf2ad5b30c
Emi Castro <emicastro1993@gmail.com>
2017-09-14 23:55:18 -0300

    Solución de bugs

--- js/juego.js
+++ js/juego.js
@@ -1,43 +1,45 @@
 // Representación de la grilla. Cada nro representa a una pieza.
-// El 7 es la posición vacía
+// El 9 es la posición vacía
 var grilla = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
 ];
 
-// Acá vamos a ir guardando la posición vacía
+// Ac&aacute; vamos a ir guardando la posición vacía
 var posicionVacia = {
   fila:2,
   columna:2
 };
 
-// Esta función va a chequear si el Rompecabezas está ordenado en la posición ganadora;
+// Esta función va a chequear si el Rompecabezas est&aacute; en la posición ganadora
 function chequearSiGano(){
-  var piezas = document.getElementsByClassName("pieza");
+
+var contador = 0;
 
   for (var x = 0; x < grilla.length; x++) {
   var filas = grilla[x];
 
     for (var y = 0; y < filas.length; y++) {
-      var piezaResultado = piezas[filas[y] - 1].id;
+      var posicion = grilla[x][y];
 
-      if (piezaResultado === 'pieza' + filas[y]) {
-        console.log("Ganaste!");
-        return true;
-      } else {
+      contador++ ;
+
+      if (grilla[x][y] !== contador) {
         return false;
       }
+
+      }
     }
+    return true;
   }
-}
 
 
 
 // la hacen los alumnos, pueden mostrar el cartel como prefieran. Pero es importante que usen
 // esta función
 function mostrarCartelGanador(){
-  alert("¡Felicitaciones! Ganaste :)")
+  alert("¡Felicitaciones! Ganaste :)");
 }
 
 // Intercambia posiciones grilla y en el DOM
@@ -47,71 +49,59 @@ function intercambiarPosiciones(fila1, columna1, fila2, columna2){
     //INTERCAMBIO LÓGICO
     var posicion1 = grilla[fila1][columna1];
     var posicion2 = grilla[fila2][columna2];
-    grilla[fila1][columna1] = posicion2;
-    grilla[fila2][columna2] = posicion1;
+    var aux = posicion1;
+
+    grilla[fila1][columna1] = posicion2
+    grilla[fila2][columna2] = aux;
 
     //INTERCAMBIO EN DOM
     //Llamando a los elementos
-    var ficha1 = document.getElementById('pieza' + posicion1);
-    var ficha2 = document.getElementById('pieza' + posicion2);
-    // var imagen1 = document.getElementById('imagen' + posicion1);
-    // var imagen2 = document.getElementById('imagen' + posicion2);
 
-    //Obteniendo el padre de las imagenes
-    // var imgPadre1 = imagen1.parentNode;
-    // var imgPadre2 = imagen2.parentNode;
+    var ficha1 = document.getElementById('pieza' + grilla[fila1][columna1]);
+    var ficha2 = document.getElementById('pieza' + grilla[fila2][columna2]);
 
     // Clonando elementos
-    var clon1 = ficha1.cloneNode();
-    var clon2 = ficha2.cloneNode();
-    // var clonImg1 = imagen1.cloneNode();
-    // var clonImg2 = imagen2.cloneNode();
+    var clon1 = ficha1.cloneNode(true);
+    var clon2 = ficha2.cloneNode(true);
 
     //Reemplazando elementos por los clones
     tablero.replaceChild(clon1, ficha2);
     tablero.replaceChild(clon2, ficha1);
-    // imgPadre2.replaceChild(clonImg1, imagen2);
-    // imgPadre1.replaceChild(clonImg2, imagen1);
-  }
 
+}
 
 // Actualiza la posición de la pieza vacía
 function actualizarPosicionVacia(nuevaFila,nuevaColumna){
-  posicionVacia = posicionVacia[nuevaFila][nuevaColumna];
+
+  posicionVacia.fila = nuevaFila;
+  posicionVacia.columna = nuevaColumna;
 }
 
 
 // Para chequear si la posicón está dentro de la grilla.
 function posicionValida(fila, columna){
-  for (var x = 0; x < grilla.length; x++) {
-  var filas = grilla[x];
-
-    for (var y = 0; y < filas.length; y++) {
-      var posicion = grilla[x][y]
-      if (fila <= x && columna <= y) {
-        return true;
-      } else {
-        return false;
-      }
-    }
+  if (fila >= 0 && fila <= 2 && columna >= 0 && columna <= 2) {
+    return true;
+  } else {
+    return false;
   }
 }
 
 // Movimiento de fichas, en este caso la que se mueve es la blanca intercambiando
 // su posición con otro elemento
 function moverEnDireccion(direccion){
-
+  // debugger;
   var nuevaFilaPiezaVacia;
   var nuevaColumnaPiezaVacia;
 
   // Intercambia pieza blanca con la pieza que está arriba suyo
   if(direccion == 40){
-    nuevaFilaPiezaVacia = posicionVacia.fila-1;
+    nuevaFilaPiezaVacia = posicionVacia.fila+1;
     nuevaColumnaPiezaVacia = posicionVacia.columna;
   }
   // Intercambia pieza blanca con la pieza que está abajo suyo
   else if (direccion == 38) {
-    nuevaFilaPiezaVacia = posicionVacia.fila+1;
+    nuevaFilaPiezaVacia = posicionVacia.fila-1;
     nuevaColumnaPiezaVacia = posicionVacia.columna;
 
   }
@@ -125,6 +115,7 @@ function moverEnDireccion(direccion){
   else if (direccion == 37) {
     nuevaFilaPiezaVacia = posicionVacia.fila;
     nuevaColumnaPiezaVacia = posicionVacia.columna-1;
+
   }
 
   // Se chequea si la nueva posición es válida, si lo es, se intercambia
@@ -150,7 +141,6 @@ function mezclarPiezas(veces){
     mezclarPiezas(veces-1);
   },100);
 }
-
 
 function capturarTeclas(){
   document.body.onkeydown = (function(evento) {
